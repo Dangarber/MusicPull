e523f3b60d0f251125122c04eb64b2248df4a35b
Daniel Garber <Danielgarber7@gmail.com>
2022-09-14 19:38:03 -0400

    update

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-if(){
-    Array.array.forEach(element => {
-        
-    });
-}
+const axios = require('axios');
+
+axios.get('http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=aae51f94ff1fca06d4905b444dc74b9c&artist=Radiohead&album=OKcomputer&format=json').then(resp => {
+
+    console.log(resp.data);
+});   
